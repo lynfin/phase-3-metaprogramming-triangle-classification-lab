10073b11a53badfa8c49ab7d05853d1859a2bd0b
Lyn <lynfinman@gmail.com>
2022-07-01 22:11:47 -0400

    commit tree c64c4d6888258694a7ef43731fa1a058fe92f7d0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c, :kind
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    raise TriangleError if [a, b, c].min <= 0
+    raise TriangleError if a + b <= c || b + c <= a || a + c <= b
+
+    @kind = if a == b && b == c then :equilateral
+            elsif a == b || a == c || b == c then :isosceles
+            else
+              :scalene
+            end
+    @kind
+  end
+
+  class TriangleError < StandardError; end
 end
